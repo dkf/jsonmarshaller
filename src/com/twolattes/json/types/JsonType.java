84b715ed2a8eef3e7e9b174357ae875a10cc4f82
pascallouisperez <pascallouisperez@30b5078c-5c40-0410-b1d4-cb505aa573e9>
2008-12-14 08:14:47 +0000

    Phasing out org.json: bye bye Doug Crockford!!!
    git-svn-id: http://jsonmarshaller.googlecode.com/svn/trunk@48 30b5078c-5c40-0410-b1d4-cb505aa573e9

--- src/com/twolattes/json/types/JsonType.java
+++ src/com/twolattes/json/types/JsonType.java
@@ -1,16 +1,12 @@
 package com.twolattes.json.types;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import com.twolattes.json.Json;
 
 /**
  * JSON type to extend basic marshalling.
- * @author Pascal
- * @version 1.0
  */
 public interface JsonType<E, J extends Json.Value> {
+
   /**
    * The returned class of this type.
    * @return the returned class of this type
@@ -20,8 +16,7 @@ public interface JsonType<E, J extends Json.Value> {
   /**
    * Handles the marshalling on an instance of an entity.
    * @param entity the entity to marshall, it can never be <tt>null</tt>
-   * @return a {@link JSONArray},  a {@link JSONObject} or a base type such as
-   * {@link Integer} or {@link String} or <tt>float</tt>
+   * @return a {@link Json.Value}
    */
   public J marshall(E entity);
 
@@ -32,4 +27,5 @@ public interface JsonType<E, J extends Json.Value> {
    * @return the instance of <tt>T</tt>
    */
   public E unmarshall(J object);
+
 }
