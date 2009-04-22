2bc8d127418582fbbb78b9d288fd16e628704660
jaredjacobs <jaredjacobs@30b5078c-5c40-0410-b1d4-cb505aa573e9>
2009-04-22 14:44:30 +0000

    Cosmetic changes only -- primarily just moving to static imports of factory methods.
    git-svn-id: http://jsonmarshaller.googlecode.com/svn/trunk@91 30b5078c-5c40-0410-b1d4-cb505aa573e9

--- srctest/com/twolattes/json/JsonTest.java
+++ srctest/com/twolattes/json/JsonTest.java
@@ -2,14 +2,19 @@ package com.twolattes.json;
 
 import static com.twolattes.json.Json.FALSE;
 import static com.twolattes.json.Json.NULL;
+import static com.twolattes.json.Json.TRUE;
 import static com.twolattes.json.Json.array;
+import static com.twolattes.json.Json.booleanValue;
+import static com.twolattes.json.Json.fromHex;
+import static com.twolattes.json.Json.fromString;
+import static com.twolattes.json.Json.nullValue;
 import static com.twolattes.json.Json.number;
 import static com.twolattes.json.Json.object;
 import static com.twolattes.json.Json.string;
+import static com.twolattes.json.OrgJsonAssert.assertJsonEquals;
 import static java.lang.String.format;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -33,31 +38,31 @@ public class JsonTest {
 
   @Test
   public void writeNull() throws Exception {
-    Json.NULL.write(writer);
+    NULL.write(writer);
     assertEquals("null", writer.toString());
   }
 
   @Test
   public void writeBooleanTrue() throws Exception {
-    Json.TRUE.write(writer);
+    TRUE.write(writer);
     assertEquals("true", writer.toString());
   }
 
   @Test
   public void writeBooleanFalse() throws Exception {
-    Json.FALSE.write(writer);
+    FALSE.write(writer);
     assertEquals("false", writer.toString());
   }
 
   @Test
   public void writeNumber1() throws Exception {
-    Json.number(56.3).write(writer);
+    number(56.3).write(writer);
     assertEquals("56.3", writer.toString());
   }
 
   @Test
   public void writeNumber2() throws Exception {
-    Json.number(new BigDecimal("789798793182739721789798793182739721")).write(writer);
+    number(new BigDecimal("789798793182739721789798793182739721")).write(writer);
     assertEquals("789798793182739721789798793182739721", writer.toString());
 
     double number = new JSONArray("[" + writer.toString() + "]").getDouble(0);
@@ -66,361 +71,352 @@ public class JsonTest {
 
   @Test
   public void writeString1() throws Exception {
-    Json.string("yeah!").write(writer);
+    string("yeah!").write(writer);
     assertEquals("\"yeah!\"", writer.toString());
   }
 
   @Test
   public void writeString2() throws Exception {
-    Json.string("ye\"ah!").write(writer);
+    string("ye\"ah!").write(writer);
     assertEquals("\"ye\\\"ah!\"", writer.toString());
   }
 
   @Test
   public void writeString3() throws Exception {
-    Json.string("ye\\ah!").write(writer);
+    string("ye\\ah!").write(writer);
     assertEquals("\"ye\\\\ah!\"", writer.toString());
   }
 
   @Test
   public void writeString4() throws Exception {
-    assertEquals("\"\\b\"", Json.string("\b").toString());
-    assertEquals("\"\\f\"", Json.string("\f").toString());
-    assertEquals("\"\\n\"", Json.string("\n").toString());
-    assertEquals("\"\\r\"", Json.string("\r").toString());
-    assertEquals("\"\\t\"", Json.string("\t").toString());
+    assertEquals("\"\\b\"", string("\b").toString());
+    assertEquals("\"\\f\"", string("\f").toString());
+    assertEquals("\"\\n\"", string("\n").toString());
+    assertEquals("\"\\r\"", string("\r").toString());
+    assertEquals("\"\\t\"", string("\t").toString());
   }
 
   @Test
   public void writeArray1() throws Exception {
-    Json.array().write(writer);
+    array().write(writer);
     assertEquals("[]", writer.toString());
   }
 
   @Test
   public void writeArray2() throws Exception {
-    Json.array(Json.TRUE, Json.FALSE).write(writer);
+    array(TRUE, FALSE).write(writer);
     assertEquals("[true,false]", writer.toString());
   }
 
   @Test
   public void writeObject1() throws Exception {
-    Json.object().write(writer);
+    object().write(writer);
     assertEquals("{}", writer.toString());
   }
 
   @Test
   public void writeObject2() throws Exception {
-    Json.Object o = Json.object();
-    o.put(Json.string("a"), Json.NULL);
-    o.write(writer);
+    object(string("a"), NULL).write(writer);
     assertEquals("{\"a\":null}", writer.toString());
   }
 
   @Test
   public void writeObject3() throws Exception {
-    Json.Object o = Json.object();
-    o.put(Json.string("a"), Json.NULL);
-    o.put(Json.string("b"), Json.NULL);
-    o.write(writer);
+    object(string("a"), NULL, string("b"), NULL).write(writer);
     assertEquals("{\"a\":null,\"b\":null}", writer.toString());
   }
 
   @Test
   public void readNull1() throws Exception {
-    assertEquals(Json.NULL, Json.fromString("null"));
+    assertEquals(NULL, fromString("null"));
   }
 
   @Test
   public void readNull2() throws Exception {
-    assertEquals(Json.NULL, Json.fromString("  null "));
+    assertEquals(NULL, fromString("  null "));
   }
 
   @Test
   public void readBoolean1() throws Exception {
-    assertEquals(Json.TRUE, Json.fromString("true"));
+    assertEquals(TRUE, fromString("true"));
   }
 
   @Test
   public void readBoolean2() throws Exception {
-    assertEquals(Json.FALSE, Json.fromString("false"));
+    assertEquals(FALSE, fromString("false"));
   }
 
   @Test
   public void readBoolean3() throws Exception {
-    assertEquals(Json.FALSE, Json.fromString("\tfalse"));
+    assertEquals(FALSE, fromString("\tfalse"));
   }
 
   @Test
   public void readString1() throws Exception {
-    assertEquals(Json.string(""), Json.fromString("\"\""));
+    assertEquals(string(""), fromString("\"\""));
   }
 
   @Test
   public void readString2() throws Exception {
-    assertEquals(Json.string("a"), Json.fromString("\"a\""));
+    assertEquals(string("a"), fromString("\"a\""));
   }
 
   @Test
   public void readString3() throws Exception {
-    assertEquals(Json.string(" "), Json.fromString("\" \""));
+    assertEquals(string(" "), fromString("\" \""));
   }
 
   @Test
   public void readString4() throws Exception {
-    assertEquals(Json.string("\""), Json.fromString("\"\\\"\""));
-    assertEquals(Json.string("\\"), Json.fromString("\"\\\\\""));
-    assertEquals(Json.string("/"), Json.fromString("\"\\/\""));
-    assertEquals(Json.string("\b"), Json.fromString("\"\\b\""));
-    assertEquals(Json.string("\f"), Json.fromString("\"\\f\""));
-    assertEquals(Json.string("\n"), Json.fromString("\"\\n\""));
-    assertEquals(Json.string("\r"), Json.fromString("\"\\r\""));
-    assertEquals(Json.string("\t"), Json.fromString("\"\\t\""));
-    assertEquals(Json.string("\ua098"), Json.fromString("\"\\ua098\""));
-    assertEquals(Json.string("\u2931"), Json.fromString("\"\\u2931\""));
+    assertEquals(string("\""), fromString("\"\\\"\""));
+    assertEquals(string("\\"), fromString("\"\\\\\""));
+    assertEquals(string("/"), fromString("\"\\/\""));
+    assertEquals(string("\b"), fromString("\"\\b\""));
+    assertEquals(string("\f"), fromString("\"\\f\""));
+    assertEquals(string("\n"), fromString("\"\\n\""));
+    assertEquals(string("\r"), fromString("\"\\r\""));
+    assertEquals(string("\t"), fromString("\"\\t\""));
+    assertEquals(string("\ua098"), fromString("\"\\ua098\""));
+    assertEquals(string("\u2931"), fromString("\"\\u2931\""));
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void readString5() throws Exception {
-    Json.fromString("\"");
+    fromString("\"");
   }
 
   @Test
   public void readString6() throws Exception {
     assertEquals(
-        Json.string("can you parse me ? this aleph (\u05D0)"),
-        Json.fromString("\"can you parse me ? this aleph (\u05D0)\""));
+        string("can you parse me ? this aleph (\u05D0)"),
+        fromString("\"can you parse me ? this aleph (\u05D0)\""));
   }
 
   @Test
   public void readNumber1() throws Exception {
-    assertEquals(Json.number(1), Json.fromString("1"));
+    assertEquals(number(1), fromString("1"));
   }
 
   @Test
   public void readNumber2() throws Exception {
-    assertEquals(Json.number(-1), Json.fromString("-1"));
+    assertEquals(number(-1), fromString("-1"));
   }
 
   @Test
   public void readNumber3() throws Exception {
-    assertEquals(Json.number(0.78), Json.fromString("0.78"));
+    assertEquals(number(0.78), fromString("0.78"));
   }
 
   @Test
   public void readNumber4() throws Exception {
     // We allow multiple 0 to make the parse faster.
-    Json.fromString("00.78");
+    fromString("00.78");
   }
 
   @Test
   public void readNumber5() throws Exception {
-    assertEquals(Json.number(10), Json.fromString("1e+1"));
-    assertEquals(Json.number(10), Json.fromString("1E+1"));
-    assertEquals(Json.number(400), Json.fromString("4e2"));
-    assertEquals(Json.number(400), Json.fromString("4E2"));
-    assertEquals(Json.number(0.1), Json.fromString("1e-1"));
-    assertEquals(Json.number(0.1), Json.fromString("1E-1"));
-    assertEquals(Json.number(8), Json.fromString("8e+00"));
-    assertEquals(Json.number(9), Json.fromString("9E+0"));
-    assertEquals(Json.number(125), Json.fromString("125e-0"));
-    assertEquals(Json.number(3), Json.fromString("3E-0000"));
+    assertEquals(number(10), fromString("1e+1"));
+    assertEquals(number(10), fromString("1E+1"));
+    assertEquals(number(400), fromString("4e2"));
+    assertEquals(number(400), fromString("4E2"));
+    assertEquals(number(0.1), fromString("1e-1"));
+    assertEquals(number(0.1), fromString("1E-1"));
+    assertEquals(number(8), fromString("8e+00"));
+    assertEquals(number(9), fromString("9E+0"));
+    assertEquals(number(125), fromString("125e-0"));
+    assertEquals(number(3), fromString("3E-0000"));
   }
 
   @Test(expected = NumberFormatException.class)
   public void readNumber6() throws Exception {
-    Json.fromString("1e");
+    fromString("1e");
   }
 
   @Test(expected = NumberFormatException.class)
   public void readNumber7() throws Exception {
-    Json.fromString("1E+");
+    fromString("1E+");
   }
 
   @Test(expected = NumberFormatException.class)
   public void readNumber8() throws Exception {
-    Json.fromString("1e-");
+    fromString("1e-");
   }
 
   @Test
   public void readNumber9() throws Exception {
     assertEquals(
-        Json.number(new BigDecimal("78923187432674312231.78923187432674312231e21733")),
-        Json.fromString("78923187432674312231.78923187432674312231e21733"));
+        number(new BigDecimal("78923187432674312231.78923187432674312231e21733")),
+        fromString("78923187432674312231.78923187432674312231e21733"));
   }
 
   @Test
   public void readArray1() throws Exception {
-    assertEquals(Json.array(), Json.fromString("[]"));
+    assertEquals(array(), fromString("[]"));
   }
 
   @Test
   public void readArray2() throws Exception {
-    assertEquals(Json.array(Json.TRUE), Json.fromString("[true]"));
+    assertEquals(array(TRUE), fromString("[true]"));
   }
 
   @Test
   public void readArray3() throws Exception {
     assertEquals(
-        Json.array(Json.TRUE, Json.NULL),
-        Json.fromString("[  true , null \t]"));
+        array(TRUE, NULL),
+        fromString("[  true , null \t]"));
   }
 
   @Test
   public void readArray4() throws Exception {
     assertEquals(
-        Json.array(Json.TRUE, Json.NULL, Json.string("")),
-        Json.fromString("[  true , null \t,\"\" ]"));
+        array(TRUE, NULL, string("")),
+        fromString("[  true , null \t,\"\" ]"));
     assertEquals(
-        Json.array(Json.TRUE, Json.NULL, Json.string("")),
-        Json.fromString("[true,null,\"\"]"));
+        array(TRUE, NULL, string("")),
+        fromString("[true,null,\"\"]"));
     assertEquals(
-        Json.array(Json.TRUE, Json.NULL, Json.string("")),
-        Json.fromString("[true, null,\"\"]"));
+        array(TRUE, NULL, string("")),
+        fromString("[true, null,\"\"]"));
   }
 
   @Test
   public void readArray5() throws Exception {
     assertEquals(
-        Json.array(
-            Json.array(
-                Json.array(
-                    Json.array(Json.FALSE)))),
-        Json.fromString("[[[[false]]]]"));
+        array(
+            array(
+                array(
+                    array(FALSE)))),
+        fromString("[[[[false]]]]"));
   }
 
   @Test
   public void readArray6() throws Exception {
     assertEquals(
-        Json.array(
-            Json.array(
-                Json.array(
-                    Json.array(Json.FALSE))),
-        Json.NULL,
-        Json.array(Json.FALSE)),
-        Json.fromString("[[[[false]]],null,[false]]"));
+        array(
+            array(
+                array(
+                    array(FALSE))),
+        NULL,
+        array(FALSE)),
+        fromString("[[[[false]]],null,[false]]"));
   }
 
   @Test
   public void readArray7() throws Exception {
     assertEquals(
-        Json.array(Json.number(89), Json.number(8.7), Json.number(42)),
-        Json.fromString("[89,8.7,4.2e1]"));
+        array(number(89), number(8.7), number(42)),
+        fromString("[89,8.7,4.2e1]"));
   }
 
   @Test
   public void readObject1() throws Exception {
-    assertEquals(Json.object(), Json.fromString("{}"));
+    assertEquals(object(), fromString("{}"));
   }
 
   @Test
   public void readObject2() throws Exception {
-    Json.Object object = Json.object();
-    object.put(Json.string("a"), Json.number(5));
-    assertEquals(object, Json.fromString("{\"a\":5}"));
+    assertEquals(
+        object(string("a"), number(5)),
+        fromString("{\"a\":5}"));
   }
 
   @Test
   public void readObject3() throws Exception {
-    Json.Object object = Json.object();
-    object.put(Json.string("a"), Json.number(5));
-    object.put(Json.string("b"), Json.FALSE);
-    assertEquals(object, Json.fromString("{\"a\":5,\"b\":false}"));
+    assertEquals(
+        object(string("a"), number(5), string("b"), FALSE),
+        fromString("{\"a\":5,\"b\":false}"));
   }
 
   @Test
-  public void fromHex() {
-    assertEquals(0, Json.fromHex('0'));
-    assertEquals(3, Json.fromHex('3'));
-    assertEquals(8, Json.fromHex('8'));
-    assertEquals(9, Json.fromHex('9'));
-    assertEquals(10, Json.fromHex('a'));
-    assertEquals(15, Json.fromHex('f'));
-    assertEquals(10, Json.fromHex('A'));
-    assertEquals(15, Json.fromHex('F'));
+  public void readFromHex() {
+    assertEquals(0, fromHex('0'));
+    assertEquals(3, fromHex('3'));
+    assertEquals(8, fromHex('8'));
+    assertEquals(9, fromHex('9'));
+    assertEquals(10, fromHex('a'));
+    assertEquals(15, fromHex('f'));
+    assertEquals(10, fromHex('A'));
+    assertEquals(15, fromHex('F'));
   }
 
   @Test
   public void objectFromPairs1() {
-    assertEquals(Json.object(), Json.object(new Json.Value[0]));
+    assertEquals(object(), object(new Json.Value[0]));
   }
 
   @Test
   public void objectFromPairs2() {
     assertEquals(
-        Json.object(Json.string("a"), Json.number(1)),
-        Json.object(new Json.Value[] { Json.string("a"), Json.number(1) }));
+        object(string("a"), number(1)),
+        object(new Json.Value[] { string("a"), number(1) }));
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void objectFromPairs3() {
-    Json.object(new Json.Value[] { Json.string("a") });
+    object(new Json.Value[] { string("a") });
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void objectFromPairs4() {
-    Json.object(new Json.Value[] { Json.number(1), Json.number(1) });
+    object(new Json.Value[] { number(1), number(1) });
   }
 
   @Test
   public void objectEqualsAndHashCode1() throws Exception {
     testEqualsAndHashCode(
-        Json.object(),
-        Json.object());
+        object(),
+        object());
   }
 
   @Test
   public void objectEqualsAndHashCode2() throws Exception {
-    Json.Object o2 = Json.object();
-    o2.put(Json.string("a"), Json.booleanValue(true));
     testNotEquals(
-        Json.object(),
-        o2);
+        object(),
+        object(string("a"), booleanValue(true)));
   }
 
   @Test
   public void objectEqualsAndHashCode3() throws Exception {
-    Json.Object o1 = Json.object();
-    o1.put(Json.string("a"), Json.booleanValue(true));
-
-    Json.Object o2 = Json.object();
-    o2.put(Json.string("a"), Json.booleanValue(true));
-    testEqualsAndHashCode(o1, o2);
+    testEqualsAndHashCode(
+        object(string("a"), booleanValue(true)),
+        object(string("a"), booleanValue(true)));
   }
 
   @Test
   public void stringEqualsAndHashCode() throws Exception {
     testEqualsAndHashCode(
-        Json.string("hello"),
-        Json.string("hello"));
+        string("hello"),
+        string("hello"));
   }
 
   @Test
   public void numberEqualsAndHashCode() throws Exception {
     testEqualsAndHashCode(
-        Json.number(9.0),
-        Json.number(new BigDecimal(9.0)));
+        number(9.0),
+        number(new BigDecimal(9.0)));
     testEqualsAndHashCode(
-        Json.number(new BigDecimal("0.00")),
-        Json.number(new BigDecimal("0.0")));
+        number(new BigDecimal("0.00")),
+        number(new BigDecimal("0.0")));
   }
 
   @Test
   public void booleanEqualsAndHashCode() throws Exception {
     testEqualsAndHashCode(
-        Json.booleanValue(true),
-        Json.booleanValue(true));
+        booleanValue(true),
+        booleanValue(true));
     testEqualsAndHashCode(
-        Json.booleanValue(false),
-        Json.booleanValue(false));
-    testNotEquals(Json.booleanValue(true), Json.booleanValue(false));
+        booleanValue(false),
+        booleanValue(false));
+    testNotEquals(
+        booleanValue(true),
+        booleanValue(false));
   }
 
   @Test
   public void nullEqualsAndHashCode() throws Exception {
     testEqualsAndHashCode(
-        Json.nullValue(),
-        Json.nullValue());
+        nullValue(),
+        nullValue());
 
     testNotEquals(NULL, string("a"));
     testNotEquals(NULL, number(5));
@@ -432,7 +428,7 @@ public class JsonTest {
   private void testEqualsAndHashCode(Json.Value v1, Json.Value v2) {
     assertEquals(v1, v2);
     assertEquals(v2, v1);
-    assertTrue(v1.hashCode() == v2.hashCode());
+    assertEquals(v1.hashCode(), v2.hashCode());
   }
 
   private void testNotEquals(Json.Value v1, Json.Value v2) {
@@ -463,12 +459,12 @@ public class JsonTest {
   private void regression(int index, boolean array) throws IOException {
     Json.Value sample = Json.read(new BufferedReader(new InputStreamReader(
         JsonTest.class.getResourceAsStream(format("/com/twolattes/json/testdata/sample%s.json", index)))));
-    Json.Value sample_pretty = Json.read(new BufferedReader(new InputStreamReader(
+    Json.Value samplePretty = Json.read(new BufferedReader(new InputStreamReader(
         JsonTest.class.getResourceAsStream(format("/com/twolattes/json/testdata/sample%s_pretty.json", index)))));
 
     System.out.println(sample);
-    System.out.println(sample_pretty);
-    assertEquals(sample, sample_pretty);
+    System.out.println(samplePretty);
+    assertEquals(sample, samplePretty);
 
     Object sampleOrgJson;
     if (array) {
@@ -477,8 +473,8 @@ public class JsonTest {
       sampleOrgJson = new JSONObject(sample.toString());
     }
 
-    OrgJsonAssert.assertJsonEquals(sample, sampleOrgJson);
-    OrgJsonAssert.assertJsonEquals(sample_pretty, sampleOrgJson);
+    assertJsonEquals(sample, sampleOrgJson);
+    assertJsonEquals(samplePretty, sampleOrgJson);
   }
 
 }
